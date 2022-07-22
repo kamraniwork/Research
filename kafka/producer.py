21921ffbd5dad95600a5b68b0450c009ba2fc02e
mehran <kamraniwork2000@gmail.com>
2022-07-22 22:16:44 +0430

    Update consumer and producer

--- kafka/producer.py
+++ kafka/producer.py
@@ -1,6 +1,36 @@
-import json
 from kafka import KafkaProducer
+import json
 
-Producer = KafkaProducer(bootstrap_servers=['localhost:9092'], value_serializer=lambda x: json.dumps(x).encode('utf-8'))
 
-Producer.send('test', {'kamrani': "mehran"})
+class MessageProducer:
+    broker = ""
+    topic = ""
+    producer = None
+
+    def __init__(self, broker, topic):
+        self.broker = broker
+        self.topic = topic
+        self.producer = KafkaProducer(bootstrap_servers=self.broker,
+                                      value_serializer=lambda v: json.dumps(v).encode('utf-8'),
+                                      acks='all',
+                                      retries=3)
+
+    def send_msg(self, msg):
+        print("sending message...")
+        try:
+            future = self.producer.send(self.topic, msg)
+            self.producer.flush()
+            future.get(timeout=60)
+            print("message sent successfully...")
+            return {'status_code': 200, 'error': None}
+        except Exception as ex:
+            return ex
+
+
+broker = 'localhost:9092'
+topic = 'test'
+message_producer = MessageProducer(broker, topic)
+
+data = {'name': 'mohammad', 'email': 'mohammad@gmail.com'}
+resp = message_producer.send_msg(data)
+print(resp)
